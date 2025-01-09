765ee248b582510521e7168ba45aed2a55f4643b
guptavgn <Gupta.Vgn@gmail.com>
2025-01-09 13:54:11 +0530

    commit

---
+++ models/integration/dim_hosts.sql
@@ -0,0 +1,12 @@
+SELECT
+    host_id,
+    NVL(
+        host_name,
+        'Anonymous'
+    ) AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    {{ ref('src_hosts') }}
+    WHERE review_text is not null
